5a25cb238e996ae2666e42940e4ce8855964a9dc
Dolan <matt.dolan@capitalone.com>
2017-09-30 11:16:26 -0400

    push 9

--- Assignments/Q9a/src/Prime.java
+++ Assignments/Q9a/src/Prime.java
@@ -1,5 +1,3 @@
-package Q9;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +5,7 @@ public class Prime {
 
 	public static void main(String[] args) {
 		
+		// The below code seems overly verbose.....am learning
 		//Since int cannot be stored in Array List define an ArrayList of Integers
 		List<Integer> myList = new ArrayList<Integer>();
 		
@@ -29,9 +28,30 @@ public class Prime {
 		
 		// Move all Integers to Array List
 		for (Integer whatever : newArray) {
-			myList.add(whatever);
-			
+			myList.add(whatever);	
 		}
-				
+		
+		// Check if each value in myList ArrayList of Integers is Prime and sysout
+		for (int i1 = 1; i1 < 100; i1++) {
+			boolean check = isPrime(myList.get(i1));
+			if (check) {
+				System.out.println(myList.get(i1));
+			}
+			
+			}
+		}
+		
+	// Accept integer parameter and perform prime number check. return true if so
+		static boolean isPrime (int parm) {
+		
+			    int z;
+			    for (z = 2; z <= parm / 2; z++)  {
+			        if (parm % z == 0) {
+			            return false;
+			        }
+			    }
+			    return true;
+			}
+					
 	}
-}
+
