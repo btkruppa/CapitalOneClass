86f583b2e820255bbe3fc0fbedb430d798bc7a4c
Reddy <purushotham.reddy@capitalone.com>
2017-10-02 15:08:30 -0400

    updated q13

--- HomeWork_Puru/src/com/capitalone/q13/Q13.java
+++ HomeWork_Puru/src/com/capitalone/q13/Q13.java
@@ -3,11 +3,18 @@ package com.capitalone.q13;
 public class Q13 {
 
 	public static void main(String[] args) {
+		int zeroOneTracker = 0;
 		for (int i = 1; i <= 4; i++) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print(((i + j) % 2) + " ");
+				System.out.print(zeroOneTracker + " ");
+				if(zeroOneTracker == 0) {
+					zeroOneTracker = 1;
+				} else {
+					zeroOneTracker = 0;
+				}
 			}
 			System.out.print("\n");
 		}
 	}
+
 }
