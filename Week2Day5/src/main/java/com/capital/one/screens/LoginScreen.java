9522cf48f8987c64859e1b170897342133ffd013
Blake Kruppa <btkruppa513@gmail.com>
2017-10-06 10:53:36 -0400

    added jdbc project

---
+++ Week2Day5/src/main/java/com/capital/one/screens/LoginScreen.java
@@ -0,0 +1,28 @@
+package com.capital.one.screens;
+
+import java.util.Scanner;
+
+import com.capital.one.beans.User;
+import com.capital.one.daos.UserDao;
+import com.capital.one.daos.UserDaoJDBC;
+
+public class LoginScreen {
+	private static Scanner scan = new Scanner(System.in);
+	private static UserDao ud = new UserDaoJDBC();
+
+	public static void main(String[] args) {
+
+		System.out.println("Please enter a username: ");
+		String username = scan.nextLine();
+		System.out.println("Please enter a password: ");
+		String password = scan.nextLine();
+		User user = ud.findByUsernameAndPassword(username, password);
+		
+		if(user != null) {
+			System.out.println("successfully logged in user details " + user);
+		} else {
+			System.out.println("invalid credentials");
+		}
+
+	}
+}
