05b836eb1b96d697cd724266d3eaea9412eece8a
Crites <davidh.crites@capitalone.com>
2017-10-18 10:30:45 -0400

    some test files

--- RevatureHW_WeekOne/src/PracticeLambda/LambdaLauncher.java
+++ RevatureHW_WeekOne/src/PracticeLambda/LambdaLauncher.java
@@ -18,9 +18,15 @@ public class LambdaLauncher {
 		
 		System.out.println(li);
 		
-		Collections.sort(li, (a,b)->a.length()-b.length());
-		
-		
+		// THIS....
+		Collections.sort(li, (a,b)->{return a.length()-b.length();});  //lambda function gives the anonymous function needed to take the place of the needed method of the Comparator interface
+		System.out.println(li);										  //implementation expected here.
+		// IS THE SAME AS THIS....
+		Collections.sort(li, (a,b)->a.length()-b.length());  //shorthand for the lambda anonymous function when you only have one line...assumes an expression that gives the return
+		System.out.println(li);
+		// IS THE SAME AS THIS....
+		StringComparator myStComp = new StringComparator();  //Collections.sort will have to go look for the compare(o1,o2) method that returns o1.length()-o2.length();
+		//Collections.sort(li, myStComp); //not working yet
 		System.out.println(li);
 
 	}
