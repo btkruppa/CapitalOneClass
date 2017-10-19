9a30fd0b471f52b6675e6a6055df94e0b683d046
Blake Kruppa <btkruppa513@gmail.com>
2017-10-19 10:18:50 -0400

    added another ajax example

---
+++ ajax/src/main/webapp/static/flashcards.js
@@ -0,0 +1,31 @@
+let xhr = new XMLHttpRequest();
+
+// on success callback function
+xhr.onload = function() {
+	let flashcards = JSON.parse(this.responseText);
+	flashcards.forEach((flashcard) => {
+		document.getElementsByClassName('flashcard-body')[0].innerHTML += `
+			<tr id="${flashcard.id}">
+				<td> 
+					${flashcard.question}
+				</td>
+				<td>
+					${flashcard.answer}
+				</td>
+			</tr>
+		`;
+	})
+	
+	console.log(this.responseText) 
+};
+
+// on fail callback function
+xhr.onerror = function() { 
+	console.log("failed " + this.responseText) 
+};
+
+// specify url and type
+xhr.open('GET', '../flashcards');
+
+// send the request
+xhr.send();
